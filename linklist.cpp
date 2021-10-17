4da3a4f81fe0506d9db48fad44ad3d8210ec9d2c
Brandon Amundson <brandon.m.amundson@gmail.com>
2021-10-17 16:12:46 -0700

    First Commit

---
+++ linklist.cpp
@@ -0,0 +1,308 @@
+/*************************************************************************/ /**
+ * @file
+ *****************************************************************************/
+
+#include "linklist.h"
+
+/**************************************************************************/ /**
+ * @author Brandon Amundson
+ *
+ * @par Description:
+ * This function creates/starts the list by setting the link list to nullptr
+ *
+ *****************************************************************************/
+LinkList::LinkList()
+{
+	headptr = nullptr;
+}
+
+/**************************************************************************/ /**
+ * @author Brandon Amundson
+ *
+ * @par Description:
+ * This function deletes every member of the linked list.
+ *
+ *****************************************************************************/
+LinkList::~LinkList()
+{
+	node* temp = headptr;
+	node* temp2 = headptr;
+	while (temp2 != nullptr)
+	{
+		temp2 = temp->next;
+		delete temp;
+		temp = temp2;
+	}
+}
+
+/**
+* @author Brandon Amundson
+*
+* @par Description:
+* This is the remove function of the program.  It is used when needing to
+* delete a specific word from the list.
+*
+* @param[in]        word - the word that is to be removed from the list.
+*
+* @returns true - successfully deleted specified word.
+* @returns false - word could not be found in list and could not be deleted.
+*
+******************************************************************************/
+bool LinkList::remove(string word)
+{
+	node* temp = headptr;
+	node* prev = headptr;
+
+	while (temp != nullptr)
+	{
+		if (temp->word == word)
+		{
+			if (temp == headptr)
+			{
+				delete headptr;
+			}
+			else
+			{
+				prev->next = temp->next;
+				delete temp;
+			}
+		}
+		prev = temp;
+		temp = temp->next;
+	}
+}
+
+
+/**
+* @author Brandon Amundson
+*
+* @par Description:
+* This is the function that returns the maximum frequency of any word that is 
+* located in the list.  It is called by the print function to output the
+* words in order of frequency as they appear in the list.
+*
+*
+* @returns max - the maximum frequency of the words in the list.
+*
+******************************************************************************/
+int LinkList::getMaxFrequency()
+{
+	node* curr = headptr; // start at head
+	int max = 0; // init max at 0
+
+	while (curr != nullptr) // while not at end of list
+	{
+		if (curr->frequencyCount >= max) // if current node freq is big than max
+		{
+			max = curr->frequencyCount; // set max to current freq count
+		}
+		curr = curr->next; // move node to next 
+	}
+	return max; // return max
+}
+
+/**************************************************************************/ /**
+* @author Brandon Amundson
+*
+* @par Description:
+* This function takes in a word and searches the list for the word.  If the 
+* word is found the function will return true, else the function returns
+* false.  
+*
+* @param[in]     word - a word to search for.
+*
+* @returns true if the word is found
+* @returns false if the word is not found
+*****************************************************************************/
+bool LinkList::find(string word)
+{
+	node* temp = headptr; // start at head
+
+	while (temp != nullptr) // while not at end of list
+	{
+		if (word == temp->word) // compare word to current node word
+		{
+			return true; // true if found
+		}
+		temp = temp->next; // move to next if not found
+	}
+	return false; // return false if not in list
+}
+
+/**************************************************************************/ /**
+* @author Brandon Amundson
+*
+* @par Description:
+* This function will go through the list of words and count the amount
+* of words in the list.  The function will then return count when it is 
+* finished.  
+*
+*
+* @returns count - the number of words in the list.  
+*****************************************************************************/
+int LinkList::size()
+{
+	node* temp = headptr; // start at head
+	int count = 0; // no nodes counted yet
+
+	while (temp != nullptr) // while not at end of list
+	{
+		count++; // increment count
+		temp = temp->next; // proceed to next node
+	}
+
+	return count; // return number of nodes in list
+}
+
+/**************************************************************************/ /**
+ * @author Brandon Amundson
+ *
+ * @par Description:
+ * This function gets in a word and then searches for the word in the list. If
+ * the word is found it will increment the frequency by one and return true.
+ * If the word is not found it will return false.
+ *
+ * @param[in]     word - a word to search for and increment the frequency of.
+ *
+ * @returns true if the word is found and it increases the frequency by one
+ * @returns false if the word is not found
+ *****************************************************************************/
+bool LinkList::incrementFrequency(string word)
+{
+	node* temp = headptr;
+	int count = 0;
+	while (temp != nullptr)
+	{
+		if (word == temp->word)
+		{
+			count = temp->frequencyCount;
+			count++;
+			temp->frequencyCount = count;
+			return true;
+		}
+		temp = temp->next;
+	}
+	return false;
+}
+
+/**************************************************************************/ /**
+ * @author Samuel Patzer
+ *
+ * @par Description:
+ * This function print the list out to an output file provided by the user.
+ * It also formats and adds headers to each frequency to easily read the list.
+ *
+ * @param[in, out]     out - the output file provided by the user
+ *
+ *****************************************************************************/
+void LinkList::print(ostream &out)
+{
+	node* temp = headptr; // start at head
+	int max = getMaxFrequency(); // get highest frequency first
+	bool header = true; // start by printing header
+	int count = size(); // count all nodes in list
+
+	while (max != 0) // while words in list
+	{
+		header = true; // reset header
+		while (temp != nullptr) // while not at end of list
+		{
+			if (temp->frequencyCount == max) // if at max frequency count
+			{
+				if (header) // if header is true, print it
+				{
+					out << "\n******************************" <<
+						"****************************" << endl <<
+						"\t\t Frequency of " << max << endl <<
+						"***************************" <<
+						"*******************************" << endl;
+					header = false; // do not continue to print header
+				}
+				if (count == 3)
+				{
+					out << temp->word << endl;
+					count = 0;
+				}
+				else
+				{
+					out << temp->word << "\t";
+					count++;
+				}
+			}
+			temp = temp->next;
+		}
+		max--;
+	}
+}
+
+/**************************************************************************/
+/** 
+* @author Cody Ortega
+* 
+* @par Description: 
+* insert word one into the list at ascending value and in the same nod gives 
+* it a frequency count that will be used later in a different function. 
+* 
+* 
+* @param[in]      word - word that will be inserted into the lsit
+* 
+* @returns true - if it inserted the words
+* @return  false - if it could not insert the words
+* 
+*****************************************************************************/
+bool LinkList::insert(string word)
+{
+	node* temp = headptr;
+	node* prev = nullptr;
+	node* new_node = nullptr;
+
+	new_node = new(nothrow) node; // new node that will go into the list
+	if (new_node == nullptr)
+	{
+		return false;
+	}
+
+	new_node->word = word;
+	new_node->frequencyCount = 1;
+	new_node->next = nullptr;
+
+	if (headptr == nullptr)
+	{
+		headptr = new_node;
+		return true;
+	}
+	if (new_node->word <= headptr->word)
+	{
+		new_node->next = headptr;
+		headptr = new_node;
+		return true;
+	}
+	while (temp != nullptr && temp->word <= new_node->word)
+	{
+		prev = temp;
+		temp = temp->next;
+	}
+	new_node->next = temp;
+	prev->next = new_node;
+	return true;
+}
+
+/**************************************************************************/
+/** 
+* @author Cody Ortega
+* 
+* @par Description: 
+*
+* checks to see if the list is empty by checking to see if headptr is empty
+* 
+* 
+* @returns true - if the list is empty
+* @returns false - If the list is not empty
+* 
+*****************************************************************************/
+bool LinkList::isEmpty()
+{
+	if (headptr == nullptr)
+		return true;
+	return false;
+}
---
+++ prog1.cpp
@@ -0,0 +1,174 @@
+/*************************************************************************/ /**
+ * @file 
+ *
+ * @mainpage Word Frequency - Linked Lists
+ * 
+ * @section course_section Course Information 
+ *
+ * @author Brandon Amundson 
+ * 
+ * @date 10/17/2021
+ * 
+ * @par Professor: 
+ *         Doug Perez
+ * 
+ * @par Course: 
+ *         CSC382
+ * 
+ * @section program_section Program Information 
+ * 
+ * @details A program to help book editors and authors to view the Frequency of
+ * the words they used in their short stories. The editors believe that truly
+ * good short stories use very few words over and over. So using the program 
+ * will help the editors will prove that this is true or not and the authors can
+ * use it to avoid using the same word to many times. The program will take in a
+ * short story and then will read in word by word and remove punctuation and check
+ * to see if the word exists and if it does it increases the frequency otherwise
+ * it adds the word to the list. Once it parses the document it will output a 
+ * list with the frequency and then words with that frequency.
+ *
+ *
+ * @section compile_section Compiling and Usage 
+ *
+ * @par Compiling Instructions: 
+ *      None.
+ * 
+ * @par Usage: 
+   @verbatim  
+   c:\> prog1.exe  input.txt output.txt
+                   input.txt - a short story or paragraph
+                   output.txt - an ouput text that gives the frequency of a word
+   @endverbatim 
+ *
+ * @section todo_bugs_modification_section Todo, Bugs, and Modifications
+ * 
+ * 
+ * @par Modifications and Development Timeline: 
+   @verbatim 
+   Date          Modification 
+   ------------  -------------------------------------------------------------- 
+   Mar  4, 2015  First workday
+   Mar 17, 2015  Second workday
+   Mar 24, 2015  Third workday
+   Oct 17, 2021  Numerous modifications and improvements made for this to work
+   @endverbatim
+ *
+ *****************************************************************************/
+
+#include "linklist.h"
+#include <algorithm>
+
+bool openFiles(char* argv[], ifstream &fin, ofstream &fout);
+string removePunc(string temp);
+
+/**************************************************************************/ /**
+ * @author Brandon Amundson
+ *
+ * @par Description:
+ * Main function that is called first when the program boots. It first checks
+ * to make sure that there is a valid amount of arguments and then opens the
+ * files provided by the user. After the file successfully opens it reads a 
+ * word in and removes the punctuation and then checks to see if it can
+ * increment the frequency of the that word. If that increment fails it will
+ * insert the word. It will do this for all the words in the file provided 
+ * then it will output the list out to a file.
+ *
+ * @param[in]     argc - a count of the command line arguments used to start
+ *                       the program.
+ * @param[in]     argv - a 2d character array of each argument.  Each token
+ *                       occupies one line in the array.
+ *
+ * @returns 0 program ran successful.
+ * @returns 1 the program fails invalid command arguments given.
+ * @returns 2 the program fails because the program couldn't open a file
+ *
+ *****************************************************************************/
+int main(int argc, char* argv[])
+{
+	ifstream fin; //input stream
+	ofstream fout; //output stream
+	LinkList list; //linked list
+	string temp; //temp string read in from file
+	if (argc == 3)
+	{
+		if (openFiles(argv, fin, fout)) //if opened all files
+		{
+			while (fin >> temp) //while reading in from input file
+			{
+				temp = removePunc(temp); //remove punctuation
+				if (!list.incrementFrequency(temp))
+					//increment temp, if unable to
+				{
+					list.insert(temp); //if not able to increment, insert
+				}
+			}
+			list.print(fout); // print list when done inserting
+		}
+		else
+			return 2; // unable to open files
+	}
+	else //invalid number of arguments
+	{
+		cout << "Invalid number of commands" << endl;
+		return 1;
+	}
+	return 0;
+}
+
+/**************************************************************************/ /**
+ * @author Brandon Amundson
+ *
+ * @par Description:
+ * This function opens files
+ *
+ * @param[in]      argv - array of characters containing the file names
+ * @param[out]     fin - input file provided by the user
+ * @param[out]     fout - output file provided by the user
+ *
+ * @returns true if the files are successfully opened 
+ * @returns false if the files are not successfully opened
+ *****************************************************************************/
+bool openFiles(char* argv[], ifstream &fin, ofstream &fout)
+{
+	fin.open(argv[1]);
+	if (!fin)
+	{
+		cout << "Failed to open input file" << endl;
+		return false;
+	}
+	fout.open(argv[2]);
+	if (!fout)
+	{
+		cout << "Failed to open output file" << endl;
+		return false;
+	}
+	return true;
+}
+
+/**************************************************************************/ /**
+ * @author Brandon Amundson
+ *
+ * @par Description:
+ * This function removes the punctuation from the string and then returns the
+ * string with no punctuation.
+ *
+ * @param[in]      temp - a word containing punctuation.
+ *
+ * @returns a string containing no punctuation.
+ *****************************************************************************/
+string removePunc(string temp)
+{
+	int size = temp.size();
+
+	for (int i = 0; i < size; i++)
+	{
+		if (!isalnum(temp[i]) || !(temp[i] == 39))
+		{
+			temp.erase(i, 1);
+			size = temp.size();
+			i = i - 1;
+		}
+		temp[i] = tolower(temp[i]);
+	}
+	return temp;
+}
